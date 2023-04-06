e3bc680cf7ca855ccd00b8c8415109718265eece
achrafameur <achref.ameur1925@gmail.com>
2023-04-06 14:32:27 +0200

    generating services fron swagger

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-import SwaggerClient from "swagger-client";
 function App() {
-  async function testSwagger() {
-    const client = await SwaggerClient({
-      url: "http://localhost:3000/api-json",
-    });
-
-    const response = await client.default.getSomeData({ id: 123 });
-    console.log(client);
-  }
-  testSwagger();
   return (
     <div className="App">
       <header className="App-header">
